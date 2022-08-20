24363cea046ce168d1588c68ae14a97dcdf1cdaa
BeWorld <36823759+BeWorld2018@users.noreply.github.com>
2022-08-20 08:27:29 +0200

    MorphOS: gcc 11 + new version

--- MorphOS/NET_version.h
+++ MorphOS/NET_version.h
@@ -1,5 +1,5 @@
 #define	str(s) #s
 #define	xstr(s) str(s)
 #define	VERSION	53
-#define	REVISION	1
-#define	VERSTAG	"\0$VER: sdl2_net.library " xstr(VERSION) "." xstr(REVISION) " (" __AMIGADATE__ ") © Ilkka Lehtoranta, Bruno Peloille, Szilárd Biró"
+#define	REVISION	2
+#define	VERSTAG	"\0$VER: sdl2_net.library " xstr(VERSION) "." xstr(REVISION) " (" __AMIGADATE__ ") © Bruno Peloille, Szilárd Biró, Ilkka Lehtoranta"
